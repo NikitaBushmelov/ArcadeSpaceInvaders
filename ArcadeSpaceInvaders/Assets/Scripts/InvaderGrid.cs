4c7a404771432765449f23a128c960cee127a598
NikitaBushmelov <bushmelovnikita@gmail.com>
2024-10-23 19:19:26 +0200

    Adding Prefabs

---
+++ ArcadeSpaceInvaders/Assets/Scripts/InvaderGrid.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvaderGrid : MonoBehaviour
+{
+    public int rows = 5;
+    public int columns = 11;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
